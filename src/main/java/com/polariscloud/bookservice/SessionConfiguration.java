a393a081f9bbf5109fdfd7e68b1e7efaec35d8a7
Abdul Haleem <abdalhaleem@outlook.com>
2018-01-10 08:53:40 +0500

    initial commit

---
+++ src/main/java/com/polariscloud/bookservice/SessionConfiguration.java
@@ -0,0 +1,8 @@
+package com.polariscloud.bookservice;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@EnableRedisHttpSession
+public class SessionConfiguration extends AbstractHttpSessionApplicationInitializer {
+
+}
